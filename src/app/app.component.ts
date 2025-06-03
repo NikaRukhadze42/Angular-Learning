aca4f3c8c71d0aab06a4bb31af6a191a3f2fe34c
Nika <nikarukhadze.n@gmail.com>
2025-06-03 18:14:43 +0400

    ng-container ng-if ng-for and interface

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,11 @@
 import { Component } from '@angular/core';
 
+interface User {
+  Id: string;
+  Name: string;
+  LastName: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +13,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Angular-First-Pet-Project';
+  Users: User[] = [
+    { Id: '1', Name: 'Nika', LastName: 'Rukhadze'},
+    { Id: '2', Name: 'Elisabed', LastName: 'Tushka'},
+    { Id: '3', Name: 'Bachana', LastName: 'Yoriauli'},
+    { Id: '4', Name: 'Lela', LastName: 'Yoriauli'}
+  ]
 }
